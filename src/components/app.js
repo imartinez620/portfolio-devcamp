7a8f480a2eb64faaff0bb33075a9df9d41a3046b
imartinez620 <info.imartinez@gmail.com>
2024-08-09 08:54:43 +0200

    20240809

--- src/components/app.js
+++ src/components/app.js
@@ -16,11 +16,11 @@ import NoMatch from "./pages/no-match.js"
 
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      loggedInStatus : "NOT_LOGGED_IN"
+      loggedInStatus: "NOT_LOGGED_IN"
       //loggedIn : false introduce muchos errores
     }
 
@@ -30,15 +30,15 @@ export default class App extends Component {
     this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
 
-  handleSuccessfulLogin(){
+  handleSuccessfulLogin() {
     this.setState({
-      loggedInStatus : "LOGGED_IN"
+      loggedInStatus: "LOGGED_IN"
     })
   }
 
-  handleUnsuccessfulLogin(){
+  handleUnsuccessfulLogin() {
     this.setState({
-      loggedInStatus : "NOT_LOGGED_IN"
+      loggedInStatus: "NOT_LOGGED_IN"
     })
   }
 
@@ -48,38 +48,38 @@ export default class App extends Component {
     });
   }
 
-  checkLoginStatus(){
+  checkLoginStatus() {
     return axios.get("https://api.devcamp.space/logged_in", { withCredentials: true }
     ).then(response => {
-        console.log("logged in return", response);
-        const loggedIn = response.data.logged_in;
-        const loggedInStatus = this.state.loggedInStatus;
+      console.log("logged in return", response);
+      const loggedIn = response.data.logged_in;
+      const loggedInStatus = this.state.loggedInStatus;
 
-        if(loggedIn && loggedInStatus === "LOGGED_IN"){
-          return loggedIn;
-        }else if(loggedIn && loggedInStatus === "NOT_LOGGED_IN"){
-          this.setState({
-              loggedInStatus : "LOGGED_IN"
-          })
-        }else if(loggedIn && loggedInStatus === "LOGGED_IN"){
-          this.setState({
-              loggedInStatus : "NOT_LOGGED_IN"
-          }).catch(error =>{
-            console.log("error", error);
-          })
-        }
+      if (loggedIn && loggedInStatus === "LOGGED_IN") {
+        return loggedIn;
+      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        })
+      } else if (loggedIn && loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+        }).catch(error => {
+          console.log("error", error);
+        })
+      }
 
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.checkLoginStatus();
 
   }
 
-  autorizedPages(){
-    return[
-    <Route path="/portfolio-manager" component={PortfolioManager} />];
+  autorizedPages() {
+    return [
+      <Route key="portfolio-manager" path="/portfolio-manager" component={PortfolioManager} />];
   }
 
   getPortfolioItems() {
@@ -100,21 +100,21 @@ export default class App extends Component {
 
         <Router>
           <div>
-            <NavigationContainer 
-              loggedInStatus = {this.state.loggedInStatus}
-              handleSuccessfulLogout = {this.handleSuccessfulLogout}
+            <NavigationContainer
+              loggedInStatus={this.state.loggedInStatus}
+              handleSuccessfulLogout={this.handleSuccessfulLogout}
             />
-         
+
             <Switch>
               <Route exact path="/" component={Home} />
-              
-              <Route path="/auth" 
+
+              <Route path="/auth"
                 render={props => (
-                    <Auth 
-                      {...props} 
-                      handleSuccessfulLogin = {this.handleSuccessfulLogin}
-                      handleUnsuccessfulLogin = {this.handleUnsuccessfulLogin}
-                      />
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
                 )
                 }
               />
@@ -123,7 +123,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              {this.state.loggedInStatus === "LOGGED_IN" ? (this.autorizedPages()):null}
+              {this.state.loggedInStatus === "LOGGED_IN" ? (this.autorizedPages()) : null}
               <Route
                 exact
                 path="/portfolio/:slug"
--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
 
 export default class PortfolioManager extends Component {
     constructor(){
@@ -10,10 +11,22 @@ export default class PortfolioManager extends Component {
         this.state = {
             portfolioItems : []
         }
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem){
+        this.setState({
+            portfolioItems : [portfolioItem].concat(this.state.portfolioItems)
+        });
+    }
+
+    handleFormSubmissionError(error){
+        console.log("handleFormSubmissionError", error)
     }
 
     getPortfolioItems(){
-        axios.get("https://jordan.devcamp.space/portfolio/portfolio_items", { 
+        axios.get("https://inakimartinez.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", { 
             withCredentials: true
           }).then(response => {
             this.setState({
@@ -32,7 +45,10 @@ export default class PortfolioManager extends Component {
         return (
         <div className='portfolio-manager-wrapper'> 
             <div className='left-column'>
-                <h1>Portfolio Form...</h1>
+                <PortfolioForm 
+                    handleSuccessfulFormSubmission = {this.handleSuccessfulFormSubmission}
+                    handleFormSubmissionError = {this.handleFormSubmissionError}
+                />
             </div>
             <div className='right-column'>
                 <PortfolioSidebarList data = {this.state.portfolioItems}/>
